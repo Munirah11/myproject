2cbf373bd28aa7781b9512b94b380d7abafd1edb
Munirah11 <adlinamutalib8@gmail.com>
2021-11-04 21:41:21 +0800

    first commit

---
+++ Photo_Gallery.php
@@ -0,0 +1,100 @@
+<html>
+<head>
+    
+    <h1 style="text-align: center; ">MY PHOTO GALLERY</h1>
+    <style>
+        body {
+            background-color: #BAA973;
+            
+            
+
+        }
+        div.gallery {
+            margin: 10px;
+            border: 2px solid #75683D;
+            float: left;
+            width: 470px;
+        }
+
+        div.gallery:hover {
+            border: 2px solid #FFF2C5;
+        }
+
+        div.gallery img {
+            width: 100%;
+            height: auto;
+        }
+
+        div.desc {
+            padding: 15px;
+            text-align: center;
+        }
+    
+    </style>
+    
+</head>
+
+<body>
+    
+    <div class="gallery">
+        <a href="myfamily1.png">
+            <img src="myfamily1.png" width="600" height="400" >
+        </a>
+        <div class="desc">"Family makes a house a home" <br>
+        It is a blessing to have a happy family because not everyone in the world does. This is my family. I have a father, a mother, three younger brothers and two younger sisters. </div>
+    </div>
+    
+
+    <div class="gallery">
+        <a target="_blank" href="parents.png">
+            <img src="parents.png"  width="600" height="400">
+        </a>
+    <div class="desc">Mohd Abdul Mutalib bin Taib is my father's name and Wan Roszzaimah is my mother's name.<br>
+        My father is a lecturer at the Sultan Ahmad Shah Vocational College, Kuala Rompin. Meanwhile, my mother is a teacher who teaches preschool students at Sekolah Kebangsaan Pontian, Kuala Rompin.</div>
+    </div>
+
+    <div class="gallery">
+        <a target="_blank" href="myfamily2.png">
+            <img src="myfamily2.png"  width="600" height="400">
+    </a>
+    <div class="desc">Let's get acquainted with my siblings! <br>
+        This is my first little brother and my first good friend. Muhammad Afiq Mifdhal was named. Born on April 21st, 2002. He is a student at MSU Shah Alam who is pursuing studies in the Medicine and Surgery Bachelor Program (MBBS).</div>
+    </div>
+
+    <div class="gallery">
+        <a target="_blank" href="myfamily3.png">
+            <img src="myfamily3.png"  width="600" height="400">
+        </a>
+        <div class="desc">My second little brother <br>
+            His name is Muhammad Afkar Mursyid. One of the fiercest in the siblings after me hehe. Born on 30th September 2004 and will sit for SPM early next year. Afkar has the same interests as me, he likes to learn the basics of computer science in school.</div>
+    </div>
+
+    <div class="gallery">
+        <a target="_blank" href="myfamily.png">
+            <img src="myfamily.png"  width="600" height="400">
+        </a>
+        <div class="desc">My youngest brother.<br> 
+            His name is Muhammad Afham Munif. Afham is a Form 4 student at MRSM Muadzam Shah. He was very fond of the subject of mathematics. Our mom often told him to be an accountant, but he wanted to be a chef because he really loves to eat!</div>
+    </div>
+
+    <div class="gallery">
+        <a target="_blank" href="sisters.png">
+            <img src="sisters.png"  width="600" height="400">
+        </a>
+        <div class="desc">My two younger sisters.<br>
+            On the left is Mawarni Azzahara Batrisya and on the right is Maisarah Adani Aqilah. Mawarni is 14 years old while Aqilah is 12 years old. Their hobby is surfing the internet and watching tiktok HAHAHA!</div>
+    </div>
+
+    <div class="gallery">
+        <a target="_blank" href="bestfriend.png">
+            <img src="bestfriend.png"  width="600" height="400">
+        </a>
+        <div class="desc">My beautiful friend. <br>
+            This girl's name is Nurul Huda Dinie. She has been a good friend of mine since high school. Currently studying at UPM, Selangor in the field of Landscape Architecture. She is a good friend although sometimes annoying. More years to count with this crazy!</div>
+    </div>
+
+         
+         
+         </body>
+    
+</html>
---
+++ index.php
@@ -0,0 +1,112 @@
+<html>
+<head>
+    
+    <style>
+   
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        li a:hover:not(.active) {
+        background-color: #111;
+        }
+
+        .active {
+        background-color: #766634;
+        }
+        body {
+            background-image: url('background.png.png');
+            background-repeat: no-repeat;
+            background-attachment: fixed;  
+            background-size: cover;
+        }
+         th, td {
+                    border-style:solid;
+                    border-color: #766634;
+                    padding: 14px 16px;
+                    
+                   
+                    
+                    
+                }
+    </style>
+    
+    <meta charset = "UTF-8">
+    <title> Munirah's Homepage </title>
+    <script>
+        window.alert("Welcome to Munirah's Profile Page. Thanks for taking your time to visit my webpage. Enjoy and Thank you.");
+  </script>
+</head>
+
+    <body>
+    <ul>
+        <li><a class="active" href="index.php">Home</a></li>
+        <li><a href="Photo_Gallery.php">Gallery</a></li>
+        <li><a href="about_me.php">About me</a></li>
+    </ul>
+
+    
+        <body style="background-color:#F0F8FF;">
+    
+            <p align ="center"><img height = 200 width = 200 src = "me.png"</img></p>
+    
+            <h1 style="text-align:center;" > WELCOME TO MY PROFILE PAGE </h1>
+            
+    
+            <h2 style="text-align:center;" > Get to know me :) </h2>
+      
+            
+            <table>
+                <table style="width:100% ">
+                   
+                <tr>
+                    <th>Fullname</th>
+                    <td>Munirah Adlina Rahwani Binti Mohd Abdul Mutalib</td>
+                </tr>
+                <tr>
+                    <th>Home Address</th>
+                    <td>No.25, Lorong 2, Taman Impiana Rompin</td>
+                </tr>
+                <tr>
+                    <th>Phone Number</th>
+                    <td>011-64334458</td>
+                </tr>
+                    <tr>
+                    <th>Academic Info</th>
+                    <td>
+                        2017 - Rompin Permai High School. SPM (3A) <br>
+                        2018 - 2021 : University of Technology MARA, Raub Campus. <br>
+                        Diploma in Computer Science with CGPA : 3.06 <br>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Working Experience</th>
+                    <td>Event Planner at The Ballonian Shop (Dec 2017 - May 2018) <br>
+                    Robotics Trainee at Roboshop Technologies Sdn. Bhd <br></td>
+                    </tr>
+              
+            </table>
+
+            </table>
+        </body>
+    </body>
+
+</html>
+
+
